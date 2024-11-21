835b75086b1f5dd8708afb8a368e8936e009dc5d
Laffey-Bnnuy <mahirooyamalaffey@gmail.com>
2024-11-21 18:30:52 -0500

    fix the function analyze triangle partially, fix the not the triangle part

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -5,7 +5,7 @@
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+	if (side1+side2 <= side3 || side1 + side3 <= side2 || side3 + side2 <= side1) {
 		result = "Not a triangle";
 	}
 	else if (side1 == side2 && side1 == side3) {
